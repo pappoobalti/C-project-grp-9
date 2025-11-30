3d65dd121867f1d4e301b1e02045a5cbf018bc60
pappoobalti <cusarthak@gmail.com>
2025-11-30 13:31:35 +0530

    Update memory.c
    correction :)

--- src/memory.c
+++ src/memory.c
@@ -1,8 +1,5 @@
 #include <stdlib.h>
 #include "memory.h"
-
-// Creates and returns dynamic memory for storing 'count' Loan structures
 Loan* initializeLoanStorage(int count) {
-    Loan* loanList = (Loan*) malloc(count * sizeof(Loan));
-    return loanList;
+    return (Loan*) malloc(count * sizeof(Loan));
 }
