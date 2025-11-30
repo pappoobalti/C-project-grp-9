01c81f8b0d6733ed41c9842536a3c8dfd7fa8ebd
pappoobalti <cusarthak@gmail.com>
2025-11-30 13:19:51 +0530

    Update memory.c
    correcting a lil error in the code....

--- src/memory.c
+++ src/memory.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include "memory.h"
 
-// Creates and returns dynamic memory for storing 'n' Loan structures
-Loan* createLoanArray(int n) {
-    Loan* loanList = (Loan*) malloc(n * sizeof(Loan)); // Allocate memory for n Loan items
-    return loanList; // Returns the allocated memory to the caller
+// Creates and returns dynamic memory for storing 'count' Loan structures
+Loan* initializeLoanStorage(int count) {
+    Loan* loanList = (Loan*) malloc(count * sizeof(Loan));
+    return loanList;
 }
