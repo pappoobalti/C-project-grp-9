102aeb93a8682d04bae914c53312774dff09865e
pappoobalti <cusarthak@gmail.com>
2025-11-30 13:21:06 +0530

    Update loan.h
    correcting a lil error....

--- src/loan.h
+++ src/loan.h
@@ -1,16 +1,13 @@
 #ifndef LOAN_H
 #define LOAN_H
 
-typedef struct LoanInfo {
-    int loanID;           // Unique loan identifier
-    char borrowerName[50]; // Name of the borrower
-    float rate;           // Interest rate
-    float amount;         // Principal amount
-    int duration;         // Duration in months
+typedef struct {
+    int loanID;
+    char borrowerName[50];
+    float amount;
+    int duration; // in months
 } Loan;
 
-void inputLoan(Loan* l);// Reads loan details from user
-void displayLoan(Loan l);// Displays the stored loan information
-
 #endif
+
 
